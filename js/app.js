f699790fee610ffb1e02d384b61a15e73873d001
nmorss <nmorss@tacitknowledge.com>
2016-01-28 14:46:01 +0000

    add carousels

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 $(document).foundation();
+
+$('.slick').slick({
+mobileFirst: true,
+centerMode: true,
+dots: true,
+  infinite: false,
+  speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        centerMode: false,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]    });
+
+$('.horiztonal-slick').slick({
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+
